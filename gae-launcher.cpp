2a528751320c92805076f8ab80911f8f53bd78f9
Siddhu9090 <85753749+Siddhu9090@users.noreply.github.com>
2024-03-15 12:10:26 +0530

    Create gae-launcher.cpp

---
+++ gae-launcher.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Vantipalli Siddhu"<<endl;
+    cout<<"21BCS5507"<<endl;
+    cout<<"21BCS-CC-620-A"<<endl;
+    cout<<"mark = 28/30?"<<endl;
+    return 0;
+}
